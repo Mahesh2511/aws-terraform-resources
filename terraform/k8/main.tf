e73e6b67dad44a258347f0771ecbf9246f02aa62
Mahesh2511 <88716330+Mahesh2511@users.noreply.github.com>
2026-04-28 18:02:37 +0530

    Update main.tf

--- terraform/k8/main.tf
+++ terraform/k8/main.tf
@@ -22,7 +22,7 @@ module "eks" {
   version = "20.0.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.28"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -37,6 +37,8 @@ module "eks" {
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
+
+      ami_type = "AL2_x86_64"
     }
   }
 
